abbd49311d604132f4e7c2d5fca241ef326cc69a
Pablo Uribe <pablo.um00@gmail.com>
2023-11-10 19:41:59 -0500

    Implement Unit of Work

--- HotelsReservation.Repository/UnitOfWork/UnitOfWork.cs
+++ HotelsReservation.Repository/UnitOfWork/UnitOfWork.cs
@@ -1,11 +1,71 @@
 ﻿using System;
+using HotelsReservation.Domain.Context;
+using HotelsReservation.Repository.Contracts;
+using HotelsReservation.Repository.Implementations;
+using Microsoft.EntityFrameworkCore;
+
 namespace HotelsReservation.Repository.UnitOfWork
 {
-	public class UnitOfWork : IUnitOfWork
-	{
-		public UnitOfWork()
-		{
-		}
-	}
-}
 
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly HotelsReservationsDbContext _context;
+        private bool _disposed = false;
+        private IHotelRepository _hotelRepository;
+        private IRoomRepository _roomRepository;
+
+        public UnitOfWork(HotelsReservationsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IHotelRepository HotelRepository
+        {
+            get
+            {
+                _hotelRepository ??= new HotelRepository(_context);
+                return _hotelRepository;
+            }
+        }
+
+        public IRoomRepository RoomRepository
+        {
+            get
+            {
+                _roomRepository ??= new RoomRepository(_context);
+                return _roomRepository;
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                ex.Entries.Single().Reload();
+            }
+        }
+
+        #region IDisposable
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.DisposeAsync();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
